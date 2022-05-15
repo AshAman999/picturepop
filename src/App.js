f69851f7311a857596cf4d22194ea86cd1f8ae3f
Aman Raj <amanpie9@gmail.com>
2022-05-15 12:00:42 +0530

    styles edited

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import pexels from "./api/pexels";
 import { Card } from "react-bootstrap";
-import ReactPaginate from "react-paginate";
+// import ReactPaginate from "react-paginate";
+import { Icon } from "@material-ui/core";
+import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
 
 function App() {
   const [search, setSearch] = useState("Random");
@@ -42,9 +44,9 @@ function App() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div className="card-header main-search">
-          <div className="row">
+      <div className="search-box">
+        <form onSubmit={handleSubmit}>
+          <div className="card-header main-search">
             <div className="ml-auto">
               <input
                 onChange={(e) => setSearch(e.target.value)}
@@ -53,83 +55,63 @@ function App() {
                 type="text"
               />
             </div>
-            {/* <div className="col-12 col-md-3 col-xl-3">  
-              <input onChange={noOfPics} name="deliveryNumber" className="AutoFocus form-control" placeholder="No of Images"  
-                  type="text" />  
-          </div>   */}
-            <div className="ml-auto">
+            <div className="ml-auto submit-btn">
               <input
                 type="submit"
                 value="Search"
                 className="btn btn-primary search-btn"
               />
             </div>
+            {/* <div className="col-12 col-md-3 col-xl-3">  
+              <input onChange={noOfPics} name="deliveryNumber" className="AutoFocus form-control" placeholder="No of Images"  
+                  type="text" />  
+          </div>   */}
           </div>
-        </div>
-        <div className="container">
-          <div className="row">
-            {loading ? (
-              // Loading Indicator
-              <div className="col-12 col-md-12 col-xl-12">
-                <div className="spinner-border text-primary" role="status">
-                  <span className="sr-only">Loading...</span>
-                </div>
+        </form>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {loading ? (
+            // Loading Indicator
+            <div className="col-12 col-md-12 col-xl-12">
+              <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
               </div>
-            ) : (
-              images.map((result) => (
-                <div className="col-sm-4" key={result.id}>
-                  <Card
-                    style={{
-                      marginTop: "10px",
-                      backgroundColor: "aliceblue",
-                    }}
-                    onClick={() => {
-                      window.open(result.src.original, "_blank");
-                    }}
-                  >
-                    <Card.Img
-                      variant="top"
-                      src={result.src.landscape}
-                      alt={result.photographer}
-                    />
-                    <Card.Body>
-                      <h5 className="card-desciption">
-                        Author : <small>{result.photographer}</small>
-                      </h5>
-                    </Card.Body>
-                  </Card>
-                </div>
-              ))
-            )}
-          </div>
+            </div>
+          ) : (
+            images.map((result) => (
+              <div className="col-sm-4" key={result.id}>
+                <Card
+                  style={{
+                    marginTop: "10px",
+                    backgroundColor: "aliceblue",
+                  }}
+                  onClick={() => {
+                    window.open(result.src.original, "_blank");
+                  }}
+                >
+                  <Card.Img
+                    variant="top"
+                    src={result.src.landscape}
+                    alt={result.photographer}
+                  />
+                  <Card.Body>
+                    <h5 className="card-desciption">
+                      Author : <small>{result.photographer}</small>
+                    </h5>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))
+          )}
         </div>
-      </form>
-      <div className="col-12 col-md-12 col-xl-12">
+      </div>
+
+      <div className="bottom-btn">
         <button
-          className="btn btn-primary"
-          onClick={() => {
-            let pg = pageNo + 1;
-            setPageNo(pg);
-            try {
-              console.log(pageNo);
-              pexels
-                .get("/search", {
-                  params: { query: { search }, per_page: 9, page: pageNo },
-                })
-                .then((response) => {
-                  setImages(response.data.photos);
-                  setLoading(false);
-                });
-            } catch (error) {
-              console.log(error);
-            }
-          }}
-        >
-          Next Page
-        </button>
-        <div>Currently on Page {pageNo}</div>
-        <button
-          className="btn btn-primary"
+          className="btn btn-primary previous-page-btn"
+          style={{ marginRight: "30px" }}
           onClick={() => {
             if (pageNo === 1) return;
             else {
@@ -151,8 +133,30 @@ function App() {
             }
           }}
         >
-          Previous Page
+          <AccessAlarm />
         </button>
+        <div>Page {pageNo}</div>
+        <button
+          className="btn btn-primary next-page-btn"
+          style={{ marginLeft: "30px" }}
+          onClick={() => {
+            let pg = pageNo + 1;
+            setPageNo(pg);
+            try {
+              console.log(pageNo);
+              pexels
+                .get("/search", {
+                  params: { query: { search }, per_page: 9, page: pageNo },
+                })
+                .then((response) => {
+                  setImages(response.data.photos);
+                  setLoading(false);
+                });
+            } catch (error) {
+              console.log(error);
+            }
+          }}
+        ></button>
       </div>
     </div>
   );
